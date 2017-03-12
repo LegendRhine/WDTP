0f4ae274e7290e1717de64b3c577218129014022
LegendRhine <legendRhine@gmail.com>
2017-03-13 00:17:44 +0800

    increase the font size of popup menu for osx

--- Source/SwingLibrary/SwingLookAndFeel.h
+++ Source/SwingLibrary/SwingLookAndFeel.h
@@ -18,13 +18,25 @@ public:
     ~SwingLookAndFeel()    { }    
 
     /** Tooltip... */
-    static TextLayout layoutTooltipText (const String& text, Colour colour) noexcept;
-    Rectangle<int> getTooltipBounds (const String& tipText, Point<int> screenPos, Rectangle<int> parentArea);
-    void drawTooltip (Graphics& g, const String& text, int width, int height);
+    static TextLayout layoutTooltipText (const String& text,
+                                         Colour colour) noexcept;
     
-    /* PropertyPanel's header text */
+    Rectangle<int> getTooltipBounds (const String& tipText,
+                                     Point<int> screenPos,
+                                     Rectangle<int> parentArea) override;
+    
+    void drawTooltip (Graphics& g, const String& text,
+                      int width, int height) override;
+    
+    /** PropertyPanel's header text */
      void drawPropertyPanelSectionHeader (Graphics& g, const String& name,
-                                          bool isOpen, int width, int height);
+                                          bool isOpen, int width, int height) override;
+    
+    /** popup menu */
+    virtual Font getPopupMenuFont() override
+    {
+        return SwingUtilities::getFontSize() - 2.f;
+    }
     
 };
 
